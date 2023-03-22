73062085259b39fdc70226dc504d1f2126e9556c
charlijj <jjcharlinski@gmail.com>
2023-03-21 22:24:33 -0700

    fix

--- create_database.sql
+++ create_database.sql
@@ -7,8 +7,8 @@
 
 DROP TABLE BorrowRecords;
 DROP TABLE CommunityTools;
-DROP TABLE Borrowers;
 DROP TABLE ToolCategories;
+DROP TABLE Borrowers;
 DROP TABLE CommunityMembers;
 DROP TABLE Communities;
 
@@ -434,44 +434,44 @@ VALUES (10, 6, 'Paint sprayer', 0, 'Good condition');
 -----------------------------------------------------------------------------
 -- Insert records -----------------------------------------------------------
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (16, 12, TO_DATE('2022-02-03', 'YYYY-MM-DD'), TO_DATE('2022-02-13', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (16, 12, TO_DATE('2022-02-03', 'YYYY-MM-DD'), TO_DATE('2022-02-13', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (7, 18, TO_DATE('2022-03-17', 'YYYY-MM-DD'), TO_DATE('2022-03-24', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (7, 18, TO_DATE('2022-03-17', 'YYYY-MM-DD'), TO_DATE('2022-03-24', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (13, 5, TO_DATE('2022-04-06', 'YYYY-MM-DD'), TO_DATE('2022-04-16', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (13, 5, TO_DATE('2022-04-06', 'YYYY-MM-DD'), TO_DATE('2022-04-16', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (3, 19, TO_DATE('2022-05-12', 'YYYY-MM-DD'), TO_DATE('2022-05-22', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (3, 19, TO_DATE('2022-05-12', 'YYYY-MM-DD'), TO_DATE('2022-05-22', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (9, 1, TO_DATE('2022-06-01', 'YYYY-MM-DD'), TO_DATE('2022-06-08', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (9, 1, TO_DATE('2022-06-01', 'YYYY-MM-DD'), TO_DATE('2022-06-08', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (15, 16, TO_DATE('2022-07-05', 'YYYY-MM-DD'), TO_DATE('2022-07-12', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (15, 16, TO_DATE('2022-07-05', 'YYYY-MM-DD'), TO_DATE('2022-07-12', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (1, 3, TO_DATE('2022-08-11', 'YYYY-MM-DD'), TO_DATE('2022-08-21', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (1, 3, TO_DATE('2022-08-11', 'YYYY-MM-DD'), TO_DATE('2022-08-21', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (18, 14, TO_DATE('2022-09-02', 'YYYY-MM-DD'), TO_DATE('2022-09-12', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (18, 14, TO_DATE('2022-09-02', 'YYYY-MM-DD'), TO_DATE('2022-09-12', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (11, 21, TO_DATE('2022-10-16', 'YYYY-MM-DD'), TO_DATE('2022-10-26', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (11, 21, TO_DATE('2022-10-16', 'YYYY-MM-DD'), TO_DATE('2022-10-26', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (2, 17, TO_DATE('2022-11-05', 'YYYY-MM-DD'), TO_DATE('2022-11-12', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (2, 17, TO_DATE('2022-11-05', 'YYYY-MM-DD'), TO_DATE('2022-11-12', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (6, 10, TO_DATE('2022-12-01', 'YYYY-MM-DD'), TO_DATE('2022-12-11', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (6, 10, TO_DATE('2022-12-01', 'YYYY-MM-DD'), TO_DATE('2022-12-11', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (20, 8, TO_DATE('2023-01-09', 'YYYY-MM-DD'), TO_DATE('2023-01-19', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (20, 8, TO_DATE('2023-01-09', 'YYYY-MM-DD'), TO_DATE('2023-01-19', 'YYYY-MM-DD'));
 
-INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
-VALUES (4, 2, TO_DATE('2023-02-02', 'YYYY-MM-DD'), TO_DATE('2023-02-09', 'YYYY-MM-DD'));
+-- INSERT INTO BorrowRecords (memberID, toolID, rentDate, returnDate)
+-- VALUES (4, 2, TO_DATE('2023-02-02', 'YYYY-MM-DD'), TO_DATE('2023-02-09', 'YYYY-MM-DD'));
 -----------------------------------------------------------------------------
 
 commit;
